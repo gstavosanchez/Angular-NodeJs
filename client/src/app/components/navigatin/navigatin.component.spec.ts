61c04869d92a231720bcff39b0f8aaf0e8e23939
Gustavo_Sanchez <gustavosanchez10059@gmail.com>
2020-01-06 15:26:20 -0600

    Primer Commit Angular y Nodejs

---
+++ client/src/app/components/navigatin/navigatin.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavigatinComponent } from './navigatin.component';
+
+describe('NavigatinComponent', () => {
+  let component: NavigatinComponent;
+  let fixture: ComponentFixture<NavigatinComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NavigatinComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavigatinComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
